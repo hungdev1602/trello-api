f4b13e371a42319b415934f1ad5bbe2c1d95de55
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-27 13:37:07 +0300

    setting router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,19 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { boardRoutes } from "~/routes/v1/boardRoutes";
+
+const Router = express.Router();
+
+// Check APIs v1 status
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "APIs V1 are ready to use" });
+});
+
+// Boards APIs
+Router.use("/boards", boardRoutes);
+
+export const APIs_V1 = Router;
