053ff650b8f3062aa64ed7bb3c406cbf5f7d63eb
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-27 19:42:12 +0300

    middleware xử lý lỗi tập trung

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -7,12 +7,12 @@ import { StatusCodes } from "http-status-codes";
 
 const createNew = async (req, res, next) => {
   try {
-    console.log("req.body:", req.body);
-    console.log("req.query:", req.query);
-    console.log("req.params:", req.params);
-    console.log("req.files:", req.files);
-    console.log("req.cookies:", req.cookies);
-    console.log("req.jwtDecoded:", req.jwtDecoded);
+    // console.log("req.body:", req.body);
+    // console.log("req.query:", req.query);
+    // console.log("req.params:", req.params);
+    // console.log("req.files:", req.files);
+    // console.log("req.cookies:", req.cookies);
+    // console.log("req.jwtDecoded:", req.jwtDecoded);
 
     // Điều hướng dữ liệu Service
 
@@ -21,9 +21,7 @@ const createNew = async (req, res, next) => {
       .status(StatusCodes.CREATED)
       .json({ message: "POST from Controller: APIs create new boards" });
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message,
-    });
+    next(error);
   }
 };
 
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -5,6 +5,7 @@
  */
 import Joi from "joi";
 import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 
 const createNew = async (req, res, next) => {
   /**
@@ -29,9 +30,9 @@ const createNew = async (req, res, next) => {
     // Validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang Controller
     next();
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message,
-    });
+    next(
+      new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message)
+    );
   }
 };
 
