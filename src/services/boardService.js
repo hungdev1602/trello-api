6b18d963cddbb02632df58c59c5bc000855eda59
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-27 23:32:36 +0300

    finish setting api create

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -5,6 +5,7 @@
  */
 
 import { slugify } from "~/utils/formatters";
+import { boardModel } from "~/models/boardModel";
 
 const createNew = async (reqBody) => {
   try {
@@ -14,13 +15,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title),
     };
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ...
+    const createdBoard = await boardModel.createNew(newBoard);
+
+    // Lấy bản ghi board sau khi gọi (tuỳ mục đích dự án có cần bước này hay ko)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
 
     // Làm thêm các xử lý logic khác với các Collection khác tuỳ đặc thù dự án
     // Bắn email, notification về cho admin khi có 1 cái board mới dc tạo
 
     // Trả kết quả về, trong service luôn phải có return
-    return newBoard;
+    return getNewBoard;
   } catch (error) {
     throw error;
   }
