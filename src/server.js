b46d2882c7b1e6e92bb7d6bfec289d63a7206244
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-07-28 18:46:24 +0300

    Fix cors

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@
  */
 
 import express from "express";
+import cors from "cors";
+import { corsOptions } from "~/config/cors";
 import exitHook from "async-exit-hook";
 import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
 import { env } from "~/config/environment";
@@ -13,6 +15,9 @@ import { errorHandlingMiddleware } from "~/middlewares/errorHandlingMiddleware";
 
 const START_SERVER = () => {
   const app = express();
+
+  // Xử lý Cors
+  app.use(cors(corsOptions));
 
   // Enable req.body json data
   app.use(express.json());
